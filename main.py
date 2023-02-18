4b09976ebdd6216b867a658bf748d7a97f99a904
Rakesh Dash <dashrakesh652@gmail>
2023-02-18 21:31:40 +0530

    row number 12 added

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def greet(name):
+    print(f'''hey {name} nice way of committing version control,isn't it ?''')
+
+
+d = {'a': 'do'}
+print('Lol')
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    greet('Rakesh')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
